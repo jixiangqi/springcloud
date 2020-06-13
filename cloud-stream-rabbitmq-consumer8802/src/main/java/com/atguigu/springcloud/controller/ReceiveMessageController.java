c0c49c119954f6ad7e00ddabda53d6ee9393336c
jixiangqi <jixiangqi2020>
2020-06-13 11:09:02 +0800

    修改内容：spring cloud stream

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/atguigu/springcloud/controller/ReceiveMessageController.java
@@ -0,0 +1,28 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author jixiangqi
+ * @version 1.0
+ * @date 2020/6/13 10:28
+ */
+@Slf4j
+@Component
+@EnableBinding(Sink.class)
+public class ReceiveMessageController {
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    @StreamListener(Sink.INPUT)
+    public void input(Message<String> message){
+        log.info("消费者1号---------->接受到的消息是："+message.getPayload() +"\t" +serverPort);
+    }
+}
