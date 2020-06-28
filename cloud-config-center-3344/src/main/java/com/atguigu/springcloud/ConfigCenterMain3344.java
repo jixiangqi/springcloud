6b268fb6dd86b98cc1c9cb78bdc12e12df819032
jixiangqi <jixiangqi2020>
2020-06-28 09:57:08 +0800

    修改内容：springcloud-alibaba-sentinel

---
+++ cloud-config-center-3344/src/main/java/com/atguigu/springcloud/ConfigCenterMain3344.java
@@ -0,0 +1,19 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author jixiangqi
+ * @version 1.0
+ * @date 2020/6/7 10:14
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigCenterMain3344 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterMain3344.class, args);
+    }
+}
